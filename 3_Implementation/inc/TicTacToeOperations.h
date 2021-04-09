b96964ee2a5c389c9316d3efcdc2d2eae9abf88d
Balkaran Singh <balkaranmaan7@gmail.com>
2021-04-09 10:33:35 +0530

    Update TicTacToeOperations.h

--- 3_Implementation/inc/TicTacToeOperations.h
+++ 3_Implementation/inc/TicTacToeOperations.h
@@ -14,7 +14,7 @@
 * @return if player won return 1 else return 0
 */
 
-int checkRows(int grid[3][3],int playerInteger);
+int checkRows(char grid[3][3],int playerInteger);
 
 
 /**
@@ -24,7 +24,7 @@ int checkRows(int grid[3][3],int playerInteger);
 * @return if player won return 1 else return 0
 */
 
-int checkColumns(int grid[3][3],int playerInteger);
+int checkColumns(char grid[3][3],int playerInteger);
 
 
 /**
@@ -34,7 +34,7 @@ int checkColumns(int grid[3][3],int playerInteger);
 * @return if player won return 1 else return 0
 */
 
-int checkDiagonals(int grid[3][3],int playerInteger);
+int checkDiagonals(char grid[3][3],int playerInteger);
 
 
 /**
@@ -43,22 +43,38 @@ int checkDiagonals(int grid[3][3],int playerInteger);
 * @return if grid is fully filled return 1 else return 0
 */
 
-int ifGridFull(int grid[3][3]);
+int ifGridFull(char grid[3][3]);
 
 /**
 * prints the grid row by row
 * @param[in] grid which is 3x3 grid of game
 */
 
-void printGrid(int grid[3][3]);
+void printGrid(char grid[3][3]);
+
 
 
 /**
 * changes the turn from one player to another
-* @param[in] grid which is 3x3 grid of game
+* @param[in] turn integer which represents the present turn
 * @returns if turn is 1 then 2 and vice versa
 */
 
-
 int changeTurn(int turn);
 
+
+/**
+* method to play game by using input and outputs
+* @param[in] grid which is 3x3 grid of game
+* @param[in] turn integer which represents the present turn
+*/
+
+void playGame(char grid[3][3],int turn);
+
+/**
+* method to Get 'X' or 'O' according to the player number
+* @param[in] player integer representing the player number
+* @param[in] char 'X' or 'O'
+*/
+
+char getSymbol(int playerInteger);
