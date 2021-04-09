172a2386069d7e6d87eab2b8667703ef0ce2c93e
Balkaran Singh <balkaranmaan7@gmail.com>
2021-04-09 10:33:07 +0530

    Update TicTacToeOperations.c

--- 3_Implementation/src/TicTacToeOperations.c
+++ 3_Implementation/src/TicTacToeOperations.c
@@ -2,17 +2,17 @@
 
 // Implementation of CheckRows Function
 
-int checkRows(int grid[3][3],int playerInteger)
+int checkRows(char grid[3][3],int playerInteger)
 {
 	int numberOfOccurences;
-
+	char sym = getSymbol(playerInteger);
 	// Loop to go through the grid and check for the occurences of the
 	for(int i=0;i<3;i++)
 	{
 		numberOfOccurences=0;
 		for(int j=0;j<3;j++)
 		{
-			if(grid[i][j]==playerInteger)
+			if(grid[i][j]==sym)
 			{
 				numberOfOccurences++;
 			}
@@ -27,17 +27,17 @@ int checkRows(int grid[3][3],int playerInteger)
 
 
 
-int checkColumns(int grid[3][3],int playerInteger)
+int checkColumns(char grid[3][3],int playerInteger)
 {
 	int numberOfOccurences;
-
+	char sym = getSymbol(playerInteger);
 	// Loop to go through the grid and check for the occurences of the
 	for(int i=0;i<3;i++)
 	{
 		numberOfOccurences=0;
 		for(int j=0;j<3;j++)
 		{
-			if(grid[j][i]==playerInteger)
+			if(grid[j][i]==sym)
 			{
 				numberOfOccurences++;
 			}
@@ -51,12 +51,13 @@ int checkColumns(int grid[3][3],int playerInteger)
 }
 
 
-int checkDiagonals(int grid[3][3],int playerInteger)
+int checkDiagonals(char grid[3][3],int playerInteger)
 {
 	int numberOfOccurences=0;
+	char sym = getSymbol(playerInteger);
 	for(int i=0;i<3;i++)
 	{
-		if(grid[i][i] == playerInteger)
+		if(grid[i][i] == sym)
 		{
 			numberOfOccurences++;
 		}
@@ -66,7 +67,7 @@ int checkDiagonals(int grid[3][3],int playerInteger)
 		return 1;
 	}
 
-	if( (grid[2][0] == playerInteger) && (grid[0][2] == playerInteger) && (grid[1][1] == playerInteger))
+	if( (grid[2][0] == sym) && (grid[0][2] == sym) && (grid[1][1] == sym))
 	{
 		return 1;
 	}
@@ -74,14 +75,14 @@ int checkDiagonals(int grid[3][3],int playerInteger)
 }
 
 
-int ifGridFull(int grid[3][3])
+int ifGridFull(char grid[3][3])
 {
 	for(int i=0;i<3;i++)
 	{
 		for(int j=0;j<3;j++)
 		{
 			// if one cell is empty return 0 that is grid is not full
-			if(grid[i][j]==0)
+			if(grid[i][j]=='-')
 			{
 				return 0;
 			}
@@ -90,14 +91,14 @@ int ifGridFull(int grid[3][3])
 	return 1;
 }
 
-void printGrid(int grid[3][3])
+void printGrid(char grid[3][3])
 {
     printf("\n\t");
 	for(int i=0;i<3;i++)
 	{
 		for(int j=0;j<3;j++)
 		{
-			printf("%d  ",grid[i][j]);
+			printf("%c  ",grid[i][j]);
 		}
 		printf("\n\t");
 	}
@@ -111,4 +112,51 @@ int changeTurn(int turn)
 		return 2;
 	}
 	return 1;
+}
+
+void playGame(char grid[3][3],int turn)
+{
+  int playerChoiceRow,playerChoiceColumn;
+  while(1)
+    {
+    	printf("\n\nPlayer %d's turn: \n",turn);
+    	printf("Make your choice:- ");
+    	scanf("%d %d",&playerChoiceRow,&playerChoiceColumn);
+    	if(playerChoiceRow>3 || playerChoiceRow<1 || playerChoiceColumn>3 || playerChoiceColumn<1)
+    	{
+    		printf("\nINVALID Choice\nPlease select row and columns with in the range that is 1-3");
+    		continue;
+    	}
+    	if(grid[--playerChoiceRow][--playerChoiceColumn] == '-')
+    	{
+    	    if(turn == 1)
+                grid[playerChoiceRow][playerChoiceColumn] = 'X';
+            else
+                grid[playerChoiceRow][playerChoiceColumn] = 'O';
+    	}
+    	else
+    	{
+			printf("The location you given is already filled!\nPlease Select some other location");
+			continue;
+    	}
+
+    	if (checkRows(grid,turn) ==1 || checkColumns(grid,turn) == 1 || checkDiagonals(grid,turn) == 1 )
+    	{
+    		printf("\n**************************\n* Player %d won the Game  *\n**************************\n\n",turn);
+    		return;
+    	}
+    	turn = changeTurn(turn);
+    	printGrid(grid);
+    	if(ifGridFull(grid) == 1)
+        {
+            printf("Game Drawn\n");
+        }
+    }
+ }
+
+char getSymbol(int playerInteger)
+{
+    if(playerInteger == 1)
+        return 'X';
+    return 'O';
 }
